8a181bfef4474646b30e2d1ca7f56e9875b7c35f
selenali07 <selenali@live.unc.edu>
2020-11-21 16:05:22 -0500

    added functionality

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,71 @@
-import React from 'react'
+import React, { Component, useState } from 'react'
 import'./Hero.css';
 import './Navbar.css';
+import { DropdownButton, Dropdown} from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 
-function Hero() {
-    return (
+let animal1 = "dog";
+let location1= 27514;
+ 
+class Hero extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            animal: "dog",
+            location: 27514,
+        }
+    }
+ 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const data = this.state
+        console.log("data is:",data)
+        
+    }
+    handleInputChange = (event) => {
+        event.preventDefault();
+        
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        if(event.target.name == "animal"){
+            animal1= event.target.value;
+ 
+        }else if(event.target.name == "location"){
+            location1 = event.target.value;
+ 
+        }
+        Window.value ={
+            animal: animal1,
+            location: location1
+        }
+        console.log("window.value",Window.value);
+    }
+
+    render (){
+        const {animal} = this.state.animal;
+        const{location} = this.state.location;
+        return (
         <div className = 'hero-container'>
             <div className = 'hero-items'>
                 <h1>Welcome to Paw Mates</h1>
                 <p>Find the perfect pet for you!</p>
-                <input type='text' className='input' placeholder='Location'/>
-                <button className='mt-3 is-pulled-right'>Search</button>
+        <p>Animal is {animal}</p>
+                <form onSubmit ={this.handleSubmit}>
+                    <input type ="text"  placeholder='Location' name = "location" className='input' onChange = {this.handleInputChange}/>
+                    
+                    <select type ="select" className = "input" name="animal" selected = "" onChange =""placeholder = "Select Animal" onChange = {this.handleInputChange}>
+                        <option value="select">Select an Animal</option>
+                        <option value="dog">Dogs</option>
+                        <option value="cat">Cats</option>
+                    </select>
+                    <button type = "submit"className='mt-3 is-pulled-right' ><Link to = "./Matches">Search</Link></button>
+                </form>
             </div>
         </div>
-        
-    )
+        )  
+    }
 }
-
+ 
 export default Hero
--- src/components/pages/Matches.js
+++ src/components/pages/Matches.js
@@ -1,27 +1,133 @@
-import React, { useState } from "react"
-import { Card, Button, Alert } from "react-bootstrap"
+import React, { useState, useEffect, useRef, useCallback } from "react";
+//import { Card, Button, Alert } from "react-bootstrap"
 import { useAuth } from "../../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
-
-export default function Matches() {
+//import Window from "../Hero"
+//import pf from "../Api.js";
+ 
+//import axios from "axios";
+// make current user information global, to let all scripts access it
+ 
+//import { render } from "@testing-library/react";
+ 
+ 
+ function Matches() {
   const [error, setError] = useState("")
+  const [count, setCount] = useState(1);
+  const [count_2, setCount_2] = useState(0);
   const { currentUser, logout } = useAuth()
+  const [pets, updatePets] = useState([])
+  const [Dogs, updateDoggo] = useState([])
   const history = useHistory()
+  let pet = [];
+  const[sendRequest, setSendRequest] = useState(false)
+  const isMounted = useRef(true)
+  const [value, setValue] = useState([]);
+  const locations = Window.value.location;
+  const specy = Window.value.animal;
+ 
+  
+ 
+var key = 'bR9wLqSz2Z8LzgM5Fke8Wqx1TobVeNJz5OMvRfEN1JoFvKO2hi';
+var secret = '3xRQlFnGjbL2HYdRw0VLnnwcXKfhVnfHsK3j08cq';
 
-  async function handleLogout() {
-    setError("")
+let distance = 2;
+ 
+ 
+//console.log("afjkaskdjfnaijsdfnasjdfnkjsdfn",e);
+useEffect(()=>{
+  // if(sendRequest){
+  const getData = async()=>{
+ 
+  setValue(Window.value);
 
-    try {
-      await logout()
-      history.push("/login")
-    } catch {
-      setError("Error logging out")
-    }
+  const response = await fetch('https://api.petfinder.com/v2/oauth2/token', {
+      method: 'POST',
+      body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      }
+  }, []);
+  const data = await response.json();
+  // console.log(data);
+  const api = await fetch('https://api.petfinder.com/v2/animals?types=' + specy + '&location=' + locations +'&distance='+distance, {
+      headers: {
+        'Authorization': data.token_type + ' ' + data.access_token,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+   })
+  const data_2 = await api.json();
+
+ 
+  updatePets(data_2.animals.filter( array => array.species == "Cat" || array.species == "Dog"));
+  setSendRequest(false);
+   
+  };
+    getData();
+},[]);
+ 
+const getCalc = async()=>{
+  setCount(count +1);
+  setCount_2(count_2+1);
+  console.log(pets.length)
+  if(count === pets.length){
+    setCount(1);
+    setCount_2(0);
   }
-
+  console.log(count);
+  console.log(count_2);
+}
+ 
   return (
     <>
     <h1>Matches</h1>
+    
+    {pets.slice(count_2,count).map((user) =>(
+      <article id = {user.id} key={user}>
+          <div className="card">
+            <header className="card-header">
+              <h1 className="card-header-title">
+              <div className="col-sm-6 text-center">
+          <img className="img-fluid rounded-circle mt-2" src={user.photos[3]}></img>
+          </div>
+        
+      <div className="media-content">
+        <p className="title is-1">{user.name}</p>
+        <p className="subtitle is-3">{user.gender} : {user.age}</p>
+      </div>
+  
+      </h1>
+  
+          </header>
+          
+            <div className="card-content">
+ 
+              <div className="content has-text-right">
+                <p class = "has-text-left is-3">
+                Status: {user.status}
+                </p>
+                <br/>
+                <p class = "has-text-left is 3">{user.name} is a {user.breeds.primary}. {user.description}</p>
+                <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+                <br/>
+                <time datetime="2020-11-21">8:09 AM - 21 Nov 2020</time>
+              </div>
+            </div>
+            <footer className="card-footer">
+              <a href="#" className="card-footer-item" ><i className="far fa-heart"></i></a>
+              <a href="#" className="card-footer-item" onClick={getCalc}>Skip</a>
+            </footer>
+     
+          </div>
+ </article>
+ 
+      
+    ))}
+ 
     </>
   )
+    
 }
+ 
+ 
+export default Matches;
